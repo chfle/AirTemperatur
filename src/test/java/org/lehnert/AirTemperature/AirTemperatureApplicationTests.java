5df33b44a9d701cd161738859b676414e8a8ecbc
Christian Lehnert <christian@Christians-MacBook-Air.local>
2023-05-15 13:05:50 +0200

    first commit

---
+++ src/test/java/org/lehnert/AirTemperature/AirTemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lehnert.AirTemperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
