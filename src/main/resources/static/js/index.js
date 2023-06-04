5525f0c3b2833ad64355f3024a6c2bb9d7964ea7
chfle <christian.lehnert.home@gmail.com>
2023-06-04 10:20:22 +0200

    javadoc

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -16,10 +16,28 @@ const calculateAverage = (data) => {
    return sum / data.length;
 }
 
+/**
+
+ Calculates the threshold based on the given array of numbers.
+
+ @param {number[]} numbers - An array of numbers.
+
+ @returns {number} The calculated threshold value.
+ */
 function calculateThreshold(numbers) {
+// Calculate the average of the numbers
    const average = numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+// Calculate the squared differences from the average
    const squaredDifferences = numbers.map(num => Math.pow(num - average, 2));
+// Calculate the variance as the average of squared differences
    const variance = squaredDifferences.reduce((sum, squaredDiff) => sum + squaredDiff, 0) / numbers.length;
+// Calculate the standard deviation as the square root of the variance
    const standardDeviation = Math.sqrt(variance);
-   return average + 2 * standardDeviation;
+// Calculate the threshold as the average plus 1.5 times the standard deviation
+// Note: 1.5 can be replaced with a different value to adjust the difficulty
+   return average + 1.5 * standardDeviation;
 }
+
+
+
+
